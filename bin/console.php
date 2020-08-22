31bb4cb02d4d46f3dee82f9ff08c91a992b2140e
Daniel K Jones <me@danielkjones.com>
2020-08-22 11:50:57 +1000

    Wire together User functionality in container

--- bin/console.php
+++ bin/console.php
@@ -1,12 +1,13 @@
 <?php
 declare(strict_types=1);
 
+use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Input\ArgvInput;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $container = require __DIR__ . '/../config/container.php';
 
-$application = $container['application'];
+$application = $container[Application::class];
 
 $application->run(new ArgvInput());
--- config/container.php
+++ config/container.php
@@ -3,11 +3,30 @@ declare(strict_types=1);
 
 use Pimple\Container;
 use Symfony\Component\Console\Application;
+use UserRoleHierarchy\Command\GetUserCommand;
+use UserRoleHierarchy\Data\Builder\UserDataBuilder;
+use UserRoleHierarchy\Entity\Builder\UserBuilder;
+use UserRoleHierarchy\Repository\UserRepository;
 
 $container = new Container();
 
-$container['application'] = static function (Container $container) {
-    return new Application('User Role Hierarchy');
+$container[Application::class] = static function (Container $container): Application {
+    $application = new Application('User Role Hierarchy');
+
+    $application->add(new GetUserCommand($container[UserRepository::class]));
+
+    return $application;
+};
+
+$container[UserDataBuilder::class] = static function (Container $container): UserDataBuilder {
+    return new UserDataBuilder(new UserBuilder());
+};
+
+$container[UserRepository::class] = static function (Container $container): UserRepository {
+    $rawUserData = file_get_contents(__DIR__ . '/../data/users.json');
+
+    $userData = $container[UserDataBuilder::class]->build(json_decode($rawUserData, true));
+    return new UserRepository($userData);
 };
 
 return $container;
